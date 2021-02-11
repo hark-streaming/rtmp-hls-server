d6774bef77b1043ff12e2cf99cc895447b97e946
DESKTOP-8DRVUQ7\Jerem <me4jeremyboett@gmail.com>
2021-02-10 22:25:18 -0800

    added stream endpoint

--- api-server/src/index.ts
+++ api-server/src/index.ts
@@ -5,7 +5,17 @@ const app = express();
 
 // ROUTES
 app.get('/', (req,res) => {
-    res.send('you did it. you made a doodd!');
+    res.send('kevin');
+});
+
+// right now it's assuming that it's localhost. need to get docker-compose to work oof
+app.get('/stream', (req,res) => {
+    res.send('http://127.0.0.1:8080/hls/test1.m3u8');
+});
+
+app.get('/stream/:user', (req,res) => {
+    let user = req.params.user;
+    res.send('http://127.0.0.1:8080/hls/test1.m3u8');
 });
 
 app.listen(3000);
