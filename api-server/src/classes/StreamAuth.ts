250fcf7b2cae88c70540e7745c0ab02f04d16ca6
DESKTOP-8DRVUQ7\Jerem <me4jeremyboett@gmail.com>
2021-03-07 21:34:11 -0800

    added the ip

--- api-server/src/classes/StreamAuth.ts
+++ api-server/src/classes/StreamAuth.ts
@@ -120,7 +120,7 @@ class StreamAuth {
 
     // change this to our end hls endpoints
     //const streamUrl = `https://${this.cdnServer}/${hlsStream}/${username}/index.m3u8`;
-    const streamUrl = `http://nginx-server:8080/hls/${username}.m3u8`;
+    const streamUrl = `http://13.59.151.129:8080/hls/${username}.m3u8`;
     const thumbUrl  = `https://${this.cdnServer}/${thumbnail}/${username}.jpg`;
 
     await streamRef.update({
